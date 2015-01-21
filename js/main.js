89d435addcf903a7f0d5283e117449cf078ce519
Andrew Baker <agb.baker@gmail.com>
2015-01-21 16:19:41 -0700

    hg-clip detection script. push BEFORE homepage update

--- js/main.js
+++ js/main.js
@@ -48,36 +48,51 @@ $(document).ready(function() {
 		verticalCentered: true,
 		scrollingSpeed: 800,
 		recordHistory: false,
-		easingcss3: 'cubic-bezier(0.895, 0.030, 0.685, 0.220)',
-		onLeave: function(index, nextIndex, direction){
+		easingcss3: 'cubic-bezier(0.895, 0.030, 0.685, 0.220)'
+		// onLeave: function(index, nextIndex, direction){
 
-			var bodyColor = $('body')
-				upQuote = $('.active h2');
-            //after leaving section 2
-            if(index == 1 && direction =='down'){
-                bodyColor.toggleClass('go_black');
-            }
+		// 	var bodyColor = $('body')
+		// 		upQuote = $('.active h2');
+  //           //after leaving section 2
+  //           if(index == 1 && direction =='down'){
+  //               bodyColor.toggleClass('go_black');
+  //           }
 
-            else if(index == 2 && direction == 'up'){
-                bodyColor.toggleClass('go_black');
-                upQuote.toggleClass('quote_up');
-            }
-            else if(index == 2 && direction == 'down'){
-                bodyColor.toggleClass('go_black');
-            }
-            else if(index == 3 && direction == 'up'){
-                bodyColor.toggleClass('go_black');
-                upQuote.toggleClass('quote_up');
-            }
-            else if(index == 3 && direction == 'down'){
-                bodyColor.toggleClass('go_black');
-            }
-            else if(index == 4 && direction == 'up'){
-                bodyColor.toggleClass('go_black');
-                upQuote.toggleClass('quote_up');
-            }
-        }
+  //           else if(index == 2 && direction == 'up'){
+  //               bodyColor.toggleClass('go_black');
+  //               upQuote.toggleClass('quote_up');
+  //           }
+  //           else if(index == 2 && direction == 'down'){
+  //               bodyColor.toggleClass('go_black');
+  //           }
+  //           else if(index == 3 && direction == 'up'){
+  //               bodyColor.toggleClass('go_black');
+  //               upQuote.toggleClass('quote_up');
+  //           }
+  //           else if(index == 3 && direction == 'down'){
+  //               bodyColor.toggleClass('go_black');
+  //           }
+  //           else if(index == 4 && direction == 'up'){
+  //               bodyColor.toggleClass('go_black');
+  //               upQuote.toggleClass('quote_up');
+  //           }
+  //       }
 	});   
+
+	// BACKGROUND-CLIP SUPPORT DETECTION
+
+	Modernizr.addTest('backgroundclip',function() {
+
+	    var div = document.createElement('div');
+
+	    if ('backgroundClip' in div.style)
+	      return true;
+
+	    'Webkit Moz O ms Khtml'.replace(/([A-Za-z]*)/g,function(val) { 
+	      if (val+'BackgroundClip' in div.style) return true;
+	    });
+
+	  });
 
 	
 	// MOVE DOWN ON ARROW CLICK
